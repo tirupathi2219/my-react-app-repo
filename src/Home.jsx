eaee4c17a398454637728d334d5b84f9d1991e92
tirupathi2219 <tirupathi2219@gmail.com>
2023-05-06 13:22:05 +0530

    form-fields

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,37 @@
+ import {useState} from 'react'
 export default function Home() {
+    const [data,setData]=useState({gender:''})
+    const handleSubmit =(e)=>{
+        e.preventDefault()
+    }
+    const regxnum=/[0-9]/
+    const handleMobile=(e) =>{
+        console.log("6==", regxnum.test(e.key));
+        if(regxnum.test(e.key))
+        {
+           console.log("10=="); 
+        }
+        else{
+            e.preventDefault()
+        }
+    }
+    const handleGender =(e) => {
+
+        console.log('19==',e.target.checked);
+        if(e.target.checked)
+        {
+
+        }
+    }
     return (
         <div>
-            This is home page scdds
+            <form action="#">
+            <div><label>Name</label> <input type='text' required/> </div>
+            <div><label>Email</label> <input type='email' /> </div>
+            <div><label>Number</label> <input type='text' onKeyDown={(e)=>{handleMobile(e)}} /> </div>
+            <div><label>Gender</label> <input type='radio' name='gender' onChange={(e)=>{handleGender(e)}} /> <label>Male</label> <input type='radio' name='gender' onChange={(e)=>{handleGender(e)}}/> <label>Female</label> <input type='radio' name='gender' onChange={(e)=>{handleGender(e)}}/> <label>Not to Specify</label> </div>
+            <div><button type='submit' onClick={(e)=> handleSubmit(e)}>Submit</button></div>
+            </form>
         </div>
     )
 }
