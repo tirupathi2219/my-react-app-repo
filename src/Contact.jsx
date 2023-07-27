296d13ad92a53c989160e7d33aa5f2866f7edf6a
tirupathi2219 <tirupathi2219@gmail.com>
2023-07-27 09:42:42 +0530

    test-changes

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -7,60 +7,7 @@ import p3 from './assets/6.jpg'
 import styles from './Contact.module.scss'
 import { useState, useEffect } from "react";
 
-import { Link } from "react-router-dom"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import p1 from './assets/flower.jpg'
-import p2 from './assets/paint.jpg'
-import p3 from './assets/6.jpg'
-import styles from './Contact.module.scss'
-import { useState, useEffect } from "react";
-
 export default function Contact() {
-    const arr = [p1, p2, p3,p1,p3]
-    const [carousel, setCarousel] = useState(document.querySelector(`.${styles.carousel}`))
-    const [arrowicons, setArrowIcons] = useState(document.querySelectorAll(`.${styles.wrapper} i`))
-    const [firstImg, setFirstImg] = useState(document.querySelectorAll(`.${styles.carousel} img`)[0])
-   useEffect(() =>{
-    setCarousel (document.querySelector(`.${styles.carousel}`))
-    setArrowIcons(document.querySelectorAll(`.${styles.wrapper} i`))
-    setFirstImg(document.querySelectorAll(`.${styles.carousel} img`)[0])
-   },[])
-    useEffect(() => {
-        if(arrowicons)
-        {
-            arrowicons.forEach((icon) => {
-                icon.addEventListener("click", () => {
-                    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
-                })
-            })
-        }
-    },[arrowicons])
-    console.log('13==',firstImg,document.querySelector(`.${styles.carousel}`));
-    let isDragStart = false, prevPageX, prevScrollLeft;
-    let firstImgWidth = firstImg?.clientWidth +14; // getting first img width & adding 14 margin value
-   const dragStart = (e) => {
-    isDragStart = true;
-    prevPageX = e.pageX
-    prevScrollLeft = carousel.scrollLeft;
-   }
-
-    const dragging = (e) => {
-        if(!isDragStart) return ;
-        e.preventDefault();
-        let positionDiff = e.prevPageX - prevPageX
-        carousel.scrollLeft = prevScrollLeft - positionDiff;
-
-    }
-    const dragStop = () => {
-        isDragStart = false;
-    }
-    if(carousel)
-    {
-        carousel.addEventListener("mousedown", dragStart)
-    carousel.addEventListener("mousemove",dragging)
-    carousel.addEventListener("mouseup", dragStop)
-    }
     const arr = [p1, p2, p3,p1,p3]
     const [carousel, setCarousel] = useState(document.querySelector(`.${styles.carousel}`))
     const [arrowicons, setArrowIcons] = useState(document.querySelectorAll(`.${styles.wrapper} i`))
