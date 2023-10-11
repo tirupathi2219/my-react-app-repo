067f93a76a3ea10b99fe23b53ecf09aaabc12fdf
tirupathi2219 <tirupathi2219@gmail.com>
2023-10-11 11:42:18 +0530

    name-field

--- src/Home.jsx
+++ src/Home.jsx
@@ -25,7 +25,7 @@ export default function Home() {
     return (
         <div className='container'>
             <form action="#">
-            <div><label>Name</label> <input type='text' required placeholder='Enter your Name '/> </div>
+            <div className='name'><label>Name</label> <input type='text' required placeholder='Enter your Name '/> </div>
             <div><label>Email</label> <input type='email' placeholder='email' /> </div>
             <div><label>Number</label> <input type='text' onKeyDown={(e)=>{handleMobile(e)}} /> </div>
             <div><label>Gender</label> <input type='radio' name='gender' onChange={(e)=>{handleGender(e)}} /> <label>Male</label> <input type='radio' name='gender' onChange={(e)=>{handleGender(e)}}/> <label>Female</label> <input type='radio' name='gender' onChange={(e)=>{handleGender(e)}}/> <label>Not to Specify</label> </div>
