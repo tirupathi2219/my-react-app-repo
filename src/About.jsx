912674680462bd599a80b65cf2f17c7cff907ebe
tirupathi2219 <tirupathi2219@gmail.com>
2023-10-11 10:53:57 +0530

    ba-color

--- src/About.jsx
+++ src/About.jsx
@@ -1,34 +1,57 @@
-import { useEffect, useState } from "react";
+import React, { useState} from 'react';
+import styles from './About.module.scss'
 
-import p1 from './assets/flower.jpg'
-import p2 from './assets/paint.jpg'
-import p3 from './assets/6.jpg'
-import { useEffect } from 'react'
-export default function About() {
-    const [tdata, setTdata] = useState([])
-    const [mName, setmName] = useState('')
-    const [mExp, setmExp] = useState('')
-    const [isEdit, setisEdit] = useState(tdata && tdata.map(()=> false ))
-    useEffect(() => {
-        fetch("mockdata.json")
-            .then(resp => resp.json())
-            .then(data => { setTdata(data.data) })
-    }, [])
-    const handleEdit = (index, evt) => {
-        console.log('14==',isEdit);
+const  About = () => {
+    const paginationSize = 3;
+    let array =["1", "2", "3", "4", "5", "6", "7"]
+    let navItems = []
+    array.forEach((item, index) => navItems.push({"item":item,"index":index}))
+    const [navItemstoShown, setNavItemstoShown] = useState(navItems.slice(0,paginationSize))
+    const minFirst = navItems.length - paginationSize;
+    const [firstIndex, setFirstIndex]= useState(1)
+    const [lastIndex, setLastIndex]= useState(paginationSize)
+    const minLast = navItems.length - 1;
 
-    }
-    console.log('11==', tdata);
-    return (
-        <div>
-            <table>
-                <thead>
-                    <tr><th>name</th><th>Experience</th></tr>
-                </thead>
-                <tbody>
-                    {tdata.map((val, index) => <tr key={index} ><td>{isEdit[index] === true ? <input value={mName} onChange={(e)=> {setmName(e.target.value)}} /> : val.name}</td><td>{isEdit[index] === true ? <input value={mExp} onChange={(e) =>{setmExp(e.target.value)}} /> : val.experience}</td><td onClick={(e) => { handleEdit(index, e) }}>Edit</td></tr>)}
-                </tbody>
-            </table>
-        </div>
-    )
+let selected;
+
+const previousClick = () => {
+    if (firstIndex - 1 >= 0) {
+        setFirstIndex(firstIndex - 1)
+        setLastIndex((firstIndex - 1) + paginationSize)
+        let val  = navItems.slice((firstIndex - 1), (firstIndex - 1) + paginationSize);
+        setNavItemstoShown(val)
+      }
 }
+
+function nextClick() {
+    if (lastIndex < minLast) {
+    setLastIndex(lastIndex + 1)
+    
+  }
+  if (lastIndex - paginationSize <= minFirst) {
+    setFirstIndex((lastIndex + 1) - paginationSize)
+  }
+  
+  let val = navItems.slice((lastIndex + 1) - paginationSize, lastIndex + 1);
+  setNavItemstoShown(val)
+  }
+  
+ function navItemClick(item) {
+    console.log('46==');
+ }
+
+return(
+    <>
+    <div className={styles.pagination}>
+        <div onClick={previousClick}> - </div>
+        {
+            navItemstoShown.map((item) => { return <div onClick={navItemClick}>{item.item}</div>})
+        }
+        <div onClick={nextClick}> + </div>
+    </div>
+    </>
+)
+
+}
+
+export default About;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,6 @@
  import {useState} from 'react'
+ import './Home.css'
+ 
 export default function Home() {
     const [data,setData]=useState({gender:''})
     const handleSubmit =(e)=>{
@@ -24,13 +26,13 @@ export default function Home() {
         }
     }
     return (
-        <div>
+        <div className='container'>
             <form action="#">
             <div><label>Name</label> <input type='text' required placeholder='Enter your Name '/> </div>
             <div><label>Email</label> <input type='email' placeholder='email' /> </div>
             <div><label>Number</label> <input type='text' onKeyDown={(e)=>{handleMobile(e)}} /> </div>
             <div><label>Gender</label> <input type='radio' name='gender' onChange={(e)=>{handleGender(e)}} /> <label>Male</label> <input type='radio' name='gender' onChange={(e)=>{handleGender(e)}}/> <label>Female</label> <input type='radio' name='gender' onChange={(e)=>{handleGender(e)}}/> <label>Not to Specify</label> </div>
-            <div><button type='submit' onClick={(e)=> handleSubmit(e)}>Submit</button></div>
+            <div><button className='submit-button' type='submit' onClick={(e)=> handleSubmit(e)}>Submit</button></div>
             </form>
         </div>
     )
